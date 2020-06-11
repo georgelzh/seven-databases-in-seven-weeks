f74c13bbbbcb961c25a49b3c8d5b178af983467e
George Li <zhihongli@bennington.edu>
2020-06-10 22:15:50 -0700

    update day1 homework

--- mongodb/day1_homework.py
+++ mongodb/day1_homework.py
@@ -8,7 +8,6 @@ pymongo connection && pymongo regex
 https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362
 """
 import pymongo
-import re
 
 # task 1: print a JSON document containing { "hello": "world" }
 
@@ -20,7 +19,7 @@ print(helloWorld)
 # word new.
 
 # connect to the db 
-client = pymongo.MongoClient()
+client = pymongo.MongoClient("mongodb://localhost:27017/")
 
 # retrieve the db called book
 book = client.book
@@ -29,11 +28,62 @@ book = client.book
 towns = book["towns"]
 
 # finish task two
-new = towns.find({"name" : { "$regex" : '/New/' }});
+# reference: https://www.softwaretestinghelp.com/mongodb/regular-expression-in-mongodb/
 
+query = {"name" : {"$regex" : "new", "$options" : "$i" } }
+new = towns.find_one(query)
 print(new)
 
+# Task 3: Find all cities whose names contain an e and are famous for food or beer.
+# becareful about the brackets bellow, it's easy to make mistakes
+# reference for $and, $not, $or: https://www.youtube.com/watch?v=GxRx0_BPiMs
+# https://docs.mongodb.com/manual/reference/operator/query/or/
+query = {"$and": [{"name" : {"$regex" : "e"}}, 
+{ "$or": [ { "famousFor" : "food" }, {"famousFor" : "beer" } ] } ] }
 
+
+e_food_or_beer = towns.find(query)
+for t in e_food_or_beer:
+	print(t)
+
+"""
+# task 4 : Create a new database named blogger with a collection named articles.
+# Insert a new article with an author name and email, creation date, and text.
+# create db reference: https://www.w3schools.com/python/python_mongodb_create_db.asp
+# create collection reference: 
+# https://www.w3schools.com/pythonpython_mongodb_create_collection.asp
+"""
+
+from datetime import datetime
+
+# check if newdb is created, create it if not:
+dblist = client.list_database_names()
+
+# notice: new db will not be created until there is data being inserted to it.
+if "blogger" not in dblist:
+	blogger = client["blogger"]
+else:
+	blogger = client["blogger"]
+
+# check what dbs we have now
+for db in dblist:
+	print("db: ", db)	# here newdb won't be printed if its just created
+
+# create new collection called "articles"
+
+articles = blogger["articles"]
+
+# check if collection exists, "articles" won't show up below because
+# mongodb wont create it until there is data being inserted
+
+collection_names = blogger.list_collection_names()	# list of all collection
+print(collection_names)
+
+# now insert a new article in to the collection "articles"
+datetime_now = datetime.now()
+new_article = {"author_name": "Bill", "email": "bill@gmail.com", 
+"date": datetime_now, "text" : "hiiiiiiiiii, this is my first article, bye."}
+articles.insert_one(new_article)
 
 
 
