19a9c8c7e28d5c3e4c27ff649488a9554fa2627d
George Li <zhihongli@bennington.edu>
2020-06-11 17:14:10 -0700

    day 2 indexing update

---
+++ mongodb/populatePhones.js
@@ -0,0 +1,19 @@
+populatePhones = function(area, start, stop) {
+	for (var i = start; i < stop; i++) {
+		var country = 1 + ((Math.random() * 8) << 0);
+		var num = (country * 1e10) + (area * 1e7) + i;
+		var fullNumber = "+" + country + " " + area + "-" + i;
+		db.phones.insert({
+			_id : num,
+			components: {
+				country: country,
+				area: area,
+				prefix: (i* 1e-4) << 0,
+				number: i
+			},
+			display: fullNumber
+		});
+		print("inserted number: " + fullNumber);
+	}
+	print("Done!");
+}
