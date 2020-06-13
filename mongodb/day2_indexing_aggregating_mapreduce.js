2fdde360745cd14804e33c29920ac1e28ac3b0ea
georgelzh <33303558+georgelzh@users.noreply.github.com>
2020-06-13 00:00:14 -0400

    fixed typo, todo: day2 homework

--- mongodb/day2_indexing_aggregating_mapreduce.js
+++ mongodb/day2_indexing_aggregating_mapreduce.js
@@ -548,7 +548,7 @@ as inputs.
 
 reduce = function(key, values) {
 	var total = 0;
-	for (var = 0; i < values.lengthl i++) {
+	for (var i = 0; i < values.lengthl;  i++) {
 		var data = values[i];
 		if('total' in data) {
 			total += data.total;
